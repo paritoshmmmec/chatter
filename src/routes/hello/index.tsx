e920332580db41d4ec8bb138ea1035a5ff4c8acf
paritosh baghel <paritoshmmmec@gmail.com>
2022-10-21 22:12:55 -0400

    sample counter

--- src/routes/hello/index.tsx
+++ src/routes/hello/index.tsx
@@ -1,8 +1,13 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStore } from '@builder.io/qwik';
 
 export default component$(() => {
+    const state = useStore({
+        count: 0,
+    });
     return (
-    <>
-        <h1>Hello free folks!</h1>
-    </>)
+        <>
+            <h1>Number of candies I am going to eat this halloween : {state.count}</h1>
+            <button onClick$={() => state.count++}>Increment</button>
+            Count:
+        </>)
 })
